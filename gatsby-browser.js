44aa88a205da10940fdfc4391580674058333c11
Jason Lavorante <jason@curiousmedia.com>
2022-02-11 13:46:42 -0700

    Implement ITCSS styling.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./src/components/layout";
-import "./src/pages/site.scss";
+import "./src/styles/site.scss";
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import Layout from "../components/layout";
 
 const IndexPage = () => {
     return (
