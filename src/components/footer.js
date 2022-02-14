1021d965ee53b3ee722824579febd64f31ad3851
Jason Lavorante <jason@curiousmedia.com>
2022-02-14 10:39:11 -0700

    Add PDF download to footer to test static folder.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,9 @@ import * as Styles from "./footer.module.scss";
 
 const Footer = () => {
     return (
-        <footer className={Styles.footer}>Footer</footer>
+        <footer className={Styles.footer}>
+          <small>Copyright notice. <a href="/file.pdf" target="_blank">PDF download</a>.</small>
+        </footer>
     )
 }
 
