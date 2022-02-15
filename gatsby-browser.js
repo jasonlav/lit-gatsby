e399f199a89e19eb81f6c988ee06182bf5a7ecca
Jason Lavorante <jason@curiousmedia.com>
2022-02-15 14:28:13 -0700

    Add Open Sans as default font.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Layout from "./src/components/layout";
+import "@fontsource/open-sans/500.css"
+import "@fontsource/open-sans/800.css"
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
